4955fc7e54852367b089e02f84e437e15a787a93
sushant <sushant0985@gmail.com>
2020-12-13 18:56:31 -0500

    Add methods to controller to post, delete and put

--- src/main/java/com/pandeys/controller/AlienController.java
+++ src/main/java/com/pandeys/controller/AlienController.java
@@ -4,12 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,11 +28,22 @@ public class AlienController {
 		return alienJpaDao.findAll();
 	}
 	
-	@PostMapping(path = "/alien")
+	@PostMapping(path = "/alien", consumes = {"application/json"})
 	@ResponseBody
 	public Alien addAlien(@RequestBody Alien alien) {
 		alienJpaDao.save(alien);
 		return alien;
+	}
+	
+	@DeleteMapping(path = "/alien/{aid}")
+	public String deleteAlien(@PathVariable Integer aid) {
+		alienJpaDao.deleteById(aid);
+		return "Alien deleted";
+	}
+	
+	@PutMapping(path = "/alien", consumes = {"application/json"})
+	public Alien addOrUpdateAlien(@RequestBody Alien alien) {
+		return alienJpaDao.save(alien);
 	}
 	
 	@GetMapping(path = "/alien/{aid}")

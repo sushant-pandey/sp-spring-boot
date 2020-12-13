e56c6a508681c70021c016c7d955ab5379b3dcf9
sushant <sushant0985@gmail.com>
2020-12-13 18:32:12 -0500

    Changes to Alien Controller for the post method

--- src/main/java/com/pandeys/controller/AlienController.java
+++ src/main/java/com/pandeys/controller/AlienController.java
@@ -5,26 +5,37 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pandeys.dao.AlienJpaDAO;
 import com.pandeys.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 	
 	@Autowired
 	private AlienJpaDAO alienJpaDao;
 	
-	@RequestMapping(path = "/aliens")
+	@GetMapping(path = "/aliens")
 	@ResponseBody
 	public List<Alien> getAllAliens() {
 		return alienJpaDao.findAll();
 	}
 	
-	@RequestMapping(path = "/alien/{aid}")
+	@PostMapping(path = "/alien")
+	@ResponseBody
+	public Alien addAlien(@RequestBody Alien alien) {
+		alienJpaDao.save(alien);
+		return alien;
+	}
+	
+	@GetMapping(path = "/alien/{aid}")
 	@ResponseBody
 	public Optional<Alien> getAlienById(@PathVariable("aid") Integer alienId) {
 		return alienJpaDao.findById(alienId);
